d8614b30d77e311bd59c7692e5f665d5687e4c1a
Jyotish Yadav <22bcs12392@cuchd.in>
2025-07-17 11:21:00 +0530

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 function App() {
-    return <>
+    const[todos, setTodos] = useState([]);
 
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/todos")
+            .then(function(response) {
+                setTodos(response.data.todos);
+            })
+    }, [])
+    return <>
+        {todos.map(todo => <Todo key = {todo.id} title = {todo.title} descripton = {todo.description} />)}
     </>
+}
+
+function Todo({title, description}){
+    return <div>
+        <h1>{title}</h1>
+        {description}
+    </div>
 }
 
 export default App
