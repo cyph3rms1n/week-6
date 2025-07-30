2b38e53da15a9445a2e266aae1ce73859f3f42b7
Jyotish Yadav <22bcs12392@cuchd.in>
2025-07-30 11:17:50 +0530

    wkup

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,34 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useState, useEffect, useMemo } from "react"
+// import axios from "axios"
 
 function App() {
-    const [selectedId, setSelectedId] = useState(1);
+    const [counter, setCounter] = useState(0);
+    const [inputValue, setInputValue] = useState(1);
 
-    return (
-        <div>
-            <button onClick={() => setSelectedId(1)}>1</button>
-            <button onClick={() => setSelectedId(2)}>2</button>
-            <button onClick={() => setSelectedId(3)}>3</button>
-            <Todo id={selectedId} />
-        </div>
-    );
-}
+    let finalVal = useMemo(() =>{
+        let sum = 0;
+        for(let i =0; i<= inputValue; i++){
+            sum = sum + i;
+        }
+        return sum;
+    }, [inputValue]);
 
-function Todo({ id }) {
-    const [todo, setTodo] = useState({});
+    console.log(finalVal);
 
-    useEffect(() => {
-<<<<<<< HEAD
-        axios.get("")
-            .then(function(response) {
-                setTodos(response.data.todos);
-            })
-    }, [])
-    return <>
-        {todos.map(todo => <Todo key = {todo.id} title = {todo.title} descripton = {todo.description} />)}
-    </>
-}
-function Todo({title, description}){
     return <div>
-        <h1>{title}</h1>
-        {description}
+        <input onChange={ function(e) {
+            setInputValue(e.target.value);
+        }} placeholder={"find the sum from 1 to n"}></input>
+
+        <br/>
+        Sum from 1 to {inputValue} is {finalVal}
+        <br/>
+
+        <button onClick={ () => {
+            setCounter(counter + 1);
+        }}>count : {counter}</button>
+
     </div>
 }
 
-
-=======
-        axios.get("https://sum-server.100xdevs.com/todo?id=" + id)
-            .then(response => {
-                setTodo(response.data.todo);
-            });
-    }, [id]);
-
-    return (
-        <div>
-            <h1>{todo.title}</h1>
-            <h4>{todo.description}</h4>
-        </div>
-    );
-}
-
->>>>>>> 8f9f4a7 (tired)
 export default App
